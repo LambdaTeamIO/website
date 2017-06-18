aefedb626fc9a305e023aec5a21b2d05b6e77df6
Albrun, Damian <damian.ea@gmail.com>
2017-06-18 11:53:34 +0100

    Added favicons

--- src/Commands.elm
+++ src/Commands.elm
@@ -1,58 +1,68 @@
-module Commands exposing(..)
+module Commands exposing (..)
 
 import Http
 import RemoteData
-import Json.Decode as Decode 
+import Json.Decode as Decode
 import Json.Decode.Pipeline exposing (decode, required, optional)
-
 import Msgs exposing (Msg)
-import Team.Models exposing(Member)
+import Team.Models exposing (Member)
 import Projects.Models exposing (Project)
+
 
 -- PROJECTS
 
-fetchProjects: Cmd Msg
+
+fetchProjects : Cmd Msg
 fetchProjects =
-  Http.get fetchProjectsUrl projectsDecoder
-      |> RemoteData.sendRequest
-      |> Cmd.map Msgs.OnFetchProjects
+    Http.get fetchProjectsUrl projectsDecoder
+        |> RemoteData.sendRequest
+        |> Cmd.map Msgs.OnFetchProjects
 
-fetchProjectsUrl: String
+
+fetchProjectsUrl : String
 fetchProjectsUrl =
-  "http://localhost:4000/projects"
+    "http://localhost:4000/projects"
 
-projectsDecoder: Decode.Decoder (List Project)
-projectsDecoder = 
-  Decode.list projectDecoder
 
-projectDecoder: Decode.Decoder Project
-projectDecoder = 
-  decode Project
-    |> required "name" Decode.string 
-    |> required "description" Decode.string 
+projectsDecoder : Decode.Decoder (List Project)
+projectsDecoder =
+    Decode.list projectDecoder
+
+
+projectDecoder : Decode.Decoder Project
+projectDecoder =
+    decode Project
+        |> required "name" Decode.string
+        |> required "description" Decode.string
+
+
 
 -- TEAM MEMBERS
 
-fetchTeam: Cmd Msg
+
+fetchTeam : Cmd Msg
 fetchTeam =
-  Http.get fetchTeamUrl teamDecoder
-    |> RemoteData.sendRequest
-    |> Cmd.map Msgs.OnFetchTeam
+    Http.get fetchTeamUrl teamDecoder
+        |> RemoteData.sendRequest
+        |> Cmd.map Msgs.OnFetchTeam
 
-fetchTeamUrl: String
+
+fetchTeamUrl : String
 fetchTeamUrl =
-  "http://localhost:4000/team"
+    "http://localhost:4000/team"
 
-teamDecoder: Decode.Decoder (List Member)
+
+teamDecoder : Decode.Decoder (List Member)
 teamDecoder =
-  Decode.list memberDecoder
+    Decode.list memberDecoder
 
-memberDecoder: Decode.Decoder Member
+
+memberDecoder : Decode.Decoder Member
 memberDecoder =
-  decode Member
-    |> required "name" Decode.string
-    |> required "title" Decode.string
-    |> required "picture" Decode.string
-    |> required "linkedin" Decode.string
-    |> required "github" Decode.string
-    |> optional "blog" (Decode.map Just Decode.string) Nothing
+    decode Member
+        |> required "name" Decode.string
+        |> required "title" Decode.string
+        |> required "picture" Decode.string
+        |> required "linkedin" Decode.string
+        |> required "github" Decode.string
+        |> optional "blog" (Decode.map Just Decode.string) Nothing
